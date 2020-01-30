c845bbd58658444ecd7842aae41b4a0bf17f6f3b
tech-freak404 <58647230+tech-freak404@users.noreply.github.com>
2020-01-30 13:19:42 -0500

    Add files via upload

---
+++ debit.sql
@@ -0,0 +1,18 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `debit`(acc_no varchar(6),amount varchar(6),OUT current_bal varchar(10))
+BEGIN
+
+
+
+if(select *from _account)then
+ select current_balance into current_bal from _account where ac_no=acc_no;
+ 
+	if(current_bal<amount)then
+		select 'Insufficient Balance';
+	else
+		UPDATE _account SET current_balance=current_bal-amount;
+        select 'Debited';
+	end if;
+ else 
+	select 'Account Not found';
+end if;
+END
